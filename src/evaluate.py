288a686c102236383aeaa152f495afdfec4bf565
Santanu <sandutta2020@gmail.com>
2022-10-09 00:57:21 -0200

    adding plots

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,8 +4,9 @@ import json
 df =pd.read_csv("data/results.csv")
 y_true=df['Survived']
 y_pred=df['Prediction']
+# Calculating accuracy for metric 
 auc = accuracy_score(y_true, y_pred)
-
-
 with open("scores.json", 'w+') as f:
     json.dump({'auc': auc}, f)
+# dumping for plot generation
+df[['Survived','Prediction']].to_csv('plots.csv')
